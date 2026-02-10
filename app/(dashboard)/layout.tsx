02847f0da70f38d230d71aa67f66e2883616b266
fivaz <31249205+fivaz@users.noreply.github.com>
2026-02-10 23:50:33 +0100

    chore: sync browser's timezone to user's timezone automatically

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,19 +1,19 @@
 "use client";
 
 import { ReactNode } from "react";
-import { usePathname } from "next/navigation";
 
 import { AppLayout } from "@/components/app-layout";
-import { cn } from "@/lib/utils";
+import { TimezoneProvider } from "@/components/timezone-sync";
 
 type DashboardLayoutType = {
 	children: ReactNode;
 };
 
 export default function DashboardLayout({ children }: DashboardLayoutType) {
-	const pathname = usePathname();
-
-	const withPadding = true;
-
-	return <AppLayout className={cn({ "px-5 pt-12": withPadding })}>{children}</AppLayout>;
+	return (
+		<AppLayout>
+			<TimezoneProvider />
+			{children}
+		</AppLayout>
+	);
 }
